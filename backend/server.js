c3a5254486bc5b9578a48f4b18448fc26ac7f3b5
Ajay Kumar Maurya <ajkmaurya2015@gmail.com>
2022-08-13 19:19:09 +0530

    Connecting to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
 const app = require('./app')
 const dotenv = require('dotenv')
+const connectDatabase = require('./config/database')
+
+
+
+
+
 
 // setting up config file
 dotenv.config({path : "backend/config/config.env"})
+
+
+// Connecting to database
+connectDatabase()
+
 
 app.listen(process.env.PORT, () => {
    console.log(`server is listening at port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
