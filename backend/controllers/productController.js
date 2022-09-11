9a116184a805e2443e9b41b0ea6b6be69987a220
Ajay-Kumar-Maurya <ajkmaurya2015@gmail.com>
2022-09-11 10:38:23 +0530

    Search And Filter Products Funtionality Done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,6 +4,7 @@ const ErrorHandler = require('../utils/errorHandler')
 
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 
+const APIFeatures = require('../utils/apiFeatures')
 
 // Create new product   =>    /api/v1/admin/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -16,9 +17,16 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
 })
 
 
-// Get all products => /api/v1/products
+// Get all products => /api/v1/products (Initial Route)
+// Get all products => /api/v1/products?keyword=apple (Route with Search Feature)
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-   const products = await Product.find()
+   
+   const apiFeatures = new APIFeatures(Product.find(), req.query)
+                           .search()
+                           .filter()
+   // Search and Filter both opertions should be performed each time for this request
+
+   const products = await apiFeatures.query
 
    res.status(200).json({
       success: true,
---
+++ backend/utils/apiFeatures.js
@@ -0,0 +1,51 @@
+class APIFeatures {
+    constructor(query, queryStr){
+        this.query = query  // query represents database query
+        this.queryStr = queryStr    // queryStr represents URL queries
+    }
+
+    search(){
+        // ternary operator is used, if keyword exist, then only search
+        const keyword = this.queryStr.keyword ? {
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: 'i'   // i means case-insensitive
+            }
+        } : {}
+
+        // console.log(keyword)
+
+        this.query = this.query.find({...keyword})
+        return this
+    }
+
+    filter(){
+        // we store queryStr in new variable,
+        // because we don't want to modify original queryStr.
+        const queryCopy = {...this.queryStr}   // unpacking object(Spread Syntax)
+
+        // Removing fields from the query string
+        const removeFields = ['keyword', 'limit', 'page']
+        removeFields.forEach(el => delete queryCopy[el])
+
+        console.log(queryCopy);
+
+        // Advanced Filter for Range Queries
+        // 2000 <= price <= 5000, 3.5 < rating <= 5 etc.
+
+        // convert JSON query to string
+        let querystr = JSON.stringify(queryCopy)
+        querystr = querystr.replace(/\b(gt|gte|lt|lte)\b/g, match => `$${match}`)
+
+        console.log(querystr);
+
+        // convert it back to JSON format.
+        this.query = this.query.find(JSON.parse(querystr));
+        return this;
+    }
+}
+
+module.exports = APIFeatures
+
+
+// This class has been implemented for search, filter and pagination.
